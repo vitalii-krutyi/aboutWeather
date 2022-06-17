82b3a10690cc12ad1af0b39d184dfe8a7207a5c9
Vitalii Krutyi <vitalii.krutyi@avenga.com>
2022-06-17 22:15:41 +0300

    Fix displaying default data before loading weather data

--- Weather/CurrentWeather/View/CurrentWeatherViewController.swift
+++ Weather/CurrentWeather/View/CurrentWeatherViewController.swift
@@ -27,6 +27,11 @@ class CurrentWeatherViewController: UIViewController {
 
 extension CurrentWeatherViewController: CurrentWeatherViewProtocol {
     func setCurrentWeather(currentWeather: CurrentWeatherModel) {
+        cityButton.isHidden = false
+        temperatureLabel.isHidden = false
+        descriptionLabel.isHidden = false
+        weatherImageView.isHidden = false
+        
         cityButton.setTitle(currentWeather.name, for: .normal)
         
         descriptionLabel.text = "   " + (currentWeather.weather.first?.description.capitalized ?? "") + "   "
